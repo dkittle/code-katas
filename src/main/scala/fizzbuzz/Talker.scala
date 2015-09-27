d077259bf8c5710b95683d864ca9eb06a589cf2c
dkittle <don@kittle.ca>
2015-09-27 12:52:39 -0400

    Using pattern matching to ouput the appropriate text

--- src/main/scala/fizzbuzz/Talker.scala
+++ src/main/scala/fizzbuzz/Talker.scala
@@ -9,18 +9,14 @@ object Talker {
     if (i < 1)
       throw new IllegalArgumentException("I cannot speak numbers less than 1")
 
-    val sb = new StringBuilder
-
-    if (i % 3 == 0)
-      sb.append("fizz")
-
-    if (i % 5 == 0)
-      sb.append("buzz")
-
-    if (sb.length == 0)
-      sb.append(i.toString)
-    sb.mkString
+    speakNumber(i)
   }
 
+  private def speakNumber(i: Int): String = i match {
+    case x if (x % 15 == 0) => "fizzbuzz"
+    case x if (x % 5 == 0) => "buzz"
+    case x if (x % 3 == 0) => "fizz"
+    case x => x.toString
+  }
 
 }
